c486377ab7ac46d3fdb7484a9f41c10fc4700174
王朝 <wangchao05@sunlands.com>
2021-07-30 19:57:34 +0800

    封装jni

---
+++ app/src/main/cpp/fftools/ffmpeg_cmd.cpp
@@ -0,0 +1,19 @@
+#include <jni.h>
+#include "ffmpeg.h"
+//
+// Created by  wangchao on 2021/7/30.
+//
+
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_ffmpegcmd_ffmpeg_FFmpegCmd_run(JNIEnv *env, jobject thiz, jobjectArray array) {
+    int argc = env->GetArrayLength(array);
+    char *argv[argc];
+    int i;
+    for (i = 0; i < argc; i++) {
+        auto js = (jstring) env->GetObjectArrayElement(array, i);
+        argv[i] = (char *) env->GetStringUTFChars(js, 0);
+    }
+    return run(argc, argv);
+}
