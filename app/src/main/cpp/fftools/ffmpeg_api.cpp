0f65ac1d6ea887ede029b754bebbe3a8c465e60f
王朝 <wangchao05@sunlands.com>
2021-07-31 12:34:10 +0800

    添加获取基本信息的jni

---
+++ app/src/main/cpp/fftools/ffmpeg_api.cpp
@@ -0,0 +1,12 @@
+#include "jni.h"
+
+//
+// Created by  wangchao on 2021/7/30.
+//
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_ffmpegcmd_ffmpeg_FFmpegApi_open(JNIEnv *env, jobject thiz, jstring url) {
+
+
+}
---
+++ app/src/main/java/com/example/ffmpegcmd/ffmpeg/FFmpegApi.kt
@@ -0,0 +1,18 @@
+package com.example.ffmpegcmd.ffmpeg
+
+/**
+ * 封装一些处理多媒体信息的常规函数，比如打开获取视频文件的宽高，时长等信息。
+* */
+class FFmpegApi {
+
+    /**
+     * 打开视频流文件
+     * @param url 视频文件路径
+     * */
+    external fun open(url: String)
+
+
+
+
+
+}
